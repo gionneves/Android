292fd06106d7c2fd233f7edf813c26180b3ab19c
giovanni <gionneves@gmail.com>
2020-10-17 18:16:34 -0300

    Inicio do app

---
+++ app/src/main/java/app/guerreirosgames/TelaCadastro.java
@@ -0,0 +1,32 @@
+package app.guerreirosgames;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+
+        // add button
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
