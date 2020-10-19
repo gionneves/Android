f0619800be81dcc07fbbed550bc7dbeb3b9a5618
giovanni <gionneves@gmail.com>
2020-10-19 16:23:21 -0300

    Inclusão de funções

--- app/src/main/java/app/guerreirosgames/MainActivity.java
+++ app/src/main/java/app/guerreirosgames/MainActivity.java
@@ -1,10 +1,12 @@
 package app.guerreirosgames;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goTelaCadastro(View view) {
+        //lsdFun();
         startActivity(new Intent(this, TelaCadastro.class));
     }
+
+    public void goTelaMain(View view) {
+        //startActivity(new Intent(this, TelaMain.class));
+        //lsdFun();
+    }
+
+    private void lsdFun() {
+        EditText text = (EditText) findViewById(R.id.loginGetEmail);
+        String lsd = text.getText().toString();
+        System.out.println(lsd);
+        //------------------------------------//
+        TextView texts = (TextView) findViewById(R.id.textViewEsqueciSenha);
+
+        if (lsd == "lsd") {
+            texts.setText("não use drogas!");
+            System.out.println("Foi sim");
+        }
+        startActivity(new Intent(this, TelaCadastro.class));
+    }
+
 }
